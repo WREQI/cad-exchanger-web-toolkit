cb6abffe04f9c1cee01462393c50f006de91c8d6
Stanislav Stolbov <stanislavstolbov@mail.ru>
2021-05-15 12:55:59 +0300

    Add index.js with exports.

---
+++ index.js
@@ -0,0 +1,13 @@
+const Cadex = require('./lib/cadex.bundle');
+const CadexCore = require('./lib/cadex.core.bundle');
+const CadexCoreMin = require('./lib/cadex.core.min');
+const CadexMin = require('./lib/cadex.min');
+const CadexThreeMin = require('./lib/cadex.three.min');
+
+export {
+  Cadex as default,
+  CadexCore,
+  CadexCoreMin,
+  CadexMin,
+  CadexThreeMin,
+};
